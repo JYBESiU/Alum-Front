4743c8c4e81a7afd5829699495431c173f4f04f5
JYBESiU <jychoi.besiu@gmail.com>
2024-08-01 06:31:37 +0900

    splash 추가

--- src/components/ChatRoomPage/ChatMessage.tsx
+++ src/components/ChatRoomPage/ChatMessage.tsx
@@ -15,43 +15,55 @@ function ChatMessage({
   isDummy = false,
 }: ChatMessageProps) {
   const isUser = message.isUser;
+  const isFeedback = message.isFeedback;
 
   return (
     <Flex
       w={"100%"}
-      justify={isUser ? "end" : "start"}
+      justify={
+        isFeedback ? "center" : isUser ? "end" : "start"
+      }
       mb={"16px"}
     >
-      <Box
-        minW={"140px"}
-        maxW={"232px"}
-        py={"8px"}
-        px={"10px"}
-        bg={isUser ? "white" : "#E4E7EB"}
-        borderRadius={"14px"}
-      >
+      {message.isFeedback ? (
         <Text
-          color={
-            isDummy
-              ? "#b3b3b3"
-              : isUser
-              ? fontBlack
-              : chatFontColor
-          }
+          fontSize={"12px"}
+          color={"rgba(17, 17, 17, 0.60)"}
         >
           {message.content}
         </Text>
-        {!isUser && (
-          <Box
-            mt={"8px"}
-            onClick={() =>
-              handleVoicePlay?.(message.voiceUrl)
+      ) : (
+        <Box
+          minW={"140px"}
+          maxW={"232px"}
+          py={"8px"}
+          px={"10px"}
+          bg={isUser ? "white" : "#E4E7EB"}
+          borderRadius={"14px"}
+        >
+          <Text
+            color={
+              isDummy
+                ? "#b3b3b3"
+                : isUser
+                ? fontBlack
+                : chatFontColor
             }
           >
-            <VoiceIcon />
-          </Box>
-        )}
-      </Box>
+            {message.content}
+          </Text>
+          {!isUser && (
+            <Box
+              mt={"8px"}
+              onClick={() =>
+                handleVoicePlay?.(message.voiceUrl)
+              }
+            >
+              <VoiceIcon />
+            </Box>
+          )}
+        </Box>
+      )}
     </Flex>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,47 @@ export default function Document() {
           as="style"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
         />
+
+        <link
+          href="splashscreens/iphoneplus_splash.png"
+          media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphonexr_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/iphonexsmax_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipad_splash.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipadpro1_splash.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipadpro3_splash.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="splashscreens/ipadpro2_splash.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
       </Head>
       <body>
         <Main />
